e021d5df762e6acbb0705f4d067f37e6f6c337e1
youssef-ahamid <58043372+youssef-ahamid@users.noreply.github.com>
2022-06-30 17:23:03 +0200

    enpoints

--- index.js
+++ index.js
@@ -11,13 +11,12 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const mongoose = require("mongoose");
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-mongoose.connection.on("open", () => {
-  console.log("MongoDB Connected...");
-});
-mongoose.connection.on("error", (e) => {
-  console.log("MongooseError: ", e);
-});
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB Connected..."));
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
@@ -26,6 +25,52 @@ const PORT = process.env.PORT || 8000;
 
 app.get("/", (req, res) => {
   res.send("GetPromo");
+});
+
+const GetPromo = require("./middleware/GetPromo");
+const Promo = require("./models/Promo");
+const { tryCatch, s, e } = require("./helpers/utils");
+const { getPromoByCode } = require("./helpers/getters");
+
+app.post("/create-promo", GetPromo, async (req, res) => {
+  await tryCatch(async () => {
+    const promo = await new Promo(req.promo).save();
+    s("New Promocode created!", { promo }, res);
+  }, res);
+});
+
+app.get("/code/:code", async (req, res) => {
+  const { code } = req.params;
+  await tryCatch(async () => {
+    const promo = await getPromoByCode(code);
+    if (!promo) return e(404, `promocode ${code} not found.`, res);
+
+    s("Promocode found!", { promo }, res);
+  }, res);
+});
+
+app.get("/code/:code/:user_id", async (req, res) => {
+  const { code, user_id } = req.params;
+  await tryCatch(async () => {
+    const promo = await getPromoByCode(code, user_id);
+    if (!promo) return e(404, `promo with code ${code} not found.`, res);
+
+    s("Promocode found!", promo, res);
+  }, res);
+});
+
+app.post("/use-code", async (req, res) => {
+  const { code, user_id } = req.body;
+  await tryCatch(async () => {
+    let promo = await getPromoByCode(code);
+    if (!promo) return e(404, `promo with code ${code} not found.`, res);
+
+    promo = await promo.use(user_id);
+    if (!promo.used) return e(400, `${promo.message}`, res);
+
+    promo = await getPromoByCode(code, user_id); // get updated verion with user information
+    s("Promocode used!", promo, res);
+  }, res);
 });
 
 app.listen(PORT, () => {
