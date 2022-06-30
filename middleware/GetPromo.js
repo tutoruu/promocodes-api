3ccc88fab7db52bef4a68035f49d2710ba1b4b63
youssef-ahamid <58043372+youssef-ahamid@users.noreply.github.com>
2022-06-30 17:32:14 +0200

    authenticate create-promo

--- middleware/GetPromo.js
+++ middleware/GetPromo.js
@@ -1,4 +1,8 @@
 const { generateCode } = require("../helpers/generators");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const { e } = require("../helpers/utils");
 
 module.exports = async function (req, res, next) {
   let {
@@ -11,6 +15,8 @@ module.exports = async function (req, res, next) {
     discount_type,
     expiry_date,
   } = req.body;
+
+  if (req.get("x-access-token") !== process.env.AUTHORIZATION_TOKEN) return e(401, "Unautharized to create promo codes", res)
 
   if (!code) code = await generateCode();
   if (!expiry_date) expiry_date = new Date().setDate(new Date().getDate() + 7);
