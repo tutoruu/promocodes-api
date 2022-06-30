833186c6f8e3d8021f515f4a31e9bceb20e340f6
youssef-ahamid <58043372+youssef-ahamid@users.noreply.github.com>
2022-06-30 17:21:12 +0200

    add user-specific data in getPromoByCode  if user id is provided

--- helpers/getters.js
+++ helpers/getters.js
@@ -1,5 +1,14 @@
 const Promo = require("../models/Promo");
 
-export async function getPromoByCode(code) {
-    return await Promo.findOne({ code });
-}
+module.exports.getPromoByCode = async function (code, user_id) {
+  const promo = await Promo.findOne({ code });
+
+  if (!user_id) return promo;
+  else {
+    const eligible = await promo.isUserEligible(user_id);
+    const times_used = promo.getUser(user_id)?.frequency || 0;
+    const times_remaining = promo.max_uses_per_user - times_used;
+
+    return { promo, ...eligible, times_used, times_remaining };
+  }
+};
